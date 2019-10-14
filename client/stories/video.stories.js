19c8b9d7fd618fef65afcbeafa66bb4144f78140
DevelopmentByDavid <david@yrbookit.com>
2019-10-13 20:21:48 -0700

    some updates and typo fix

--- client/stories/video.stories.js
+++ client/stories/video.stories.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import VideoPlayer from '../src/videoo-player';
+import VideoPlayer from '../src/video-player';
 
 storiesOf('VideoPlayer', module).add('Video', () => <VideoPlayer />);
