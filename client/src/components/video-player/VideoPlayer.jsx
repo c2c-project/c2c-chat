4c1edf9971284a69a01fb231ce6bbd380e498ed4
smuzh001 <smuzh001@ucr.edu>
2020-01-01 14:31:17 -0800

    rough draft of clip grid

--- client/src/components/video-player/VideoPlayer.jsx
+++ client/src/components/video-player/VideoPlayer.jsx
@@ -1,11 +1,54 @@
 import React, {useState, useRef} from 'react';
 import ReactPlayer from 'react-player';
-
+import Grid from './ClipGrid';
 
 function VideoPlayer() {
     const [timeStamp, setTimeStamp] = useState(false);
     const player = useRef();
+    const grid = useRef();
     
+    const [clipState, setClipState] = useState([
+        {
+            clipTitle: 'clip1',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip2',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip4',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip5',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip6',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip7',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip8',
+            startTime: 12,
+            endTime: 14,
+        },
+        {
+            clipTitle: 'clip9',
+            startTime: 12,
+            endTime: 14,
+        },
+    ]);
 
     const handleTimeStamp = ({played, loaded, playedSeconds, loadedSeconds}) => {
         // console.log(played);
@@ -13,13 +56,18 @@ function VideoPlayer() {
         setTimeStamp(playedSeconds);
     }
 
-    const displayPlayTime = () => {
-        console.log('You clicked the button');
-        console.log(timeStamp);
+    const addToClips = () => {
+        const newClip = {
+            clipTitle: timeStamp,
+            startTime: timeStamp,
+            endTime: 15,
+        };
+        setClipState([...clipState, newClip ]);
+        
     }
 
     const handleClipEvent = () => {
-        
+        console.log('you clicked a button');
         player.current.seekTo(10, 'seconds');
         
     }
@@ -36,14 +84,17 @@ function VideoPlayer() {
                 // heigh=''
                 playsinline
                 onProgress={handleTimeStamp}
+                clipEvent={handleClipEvent}
             />
             <h1 id='header'>
                 Hello World
             </h1>
 
-            <button type='button' onClick={displayPlayTime}>Log</button>
-            <button type='button' onClick={handleClipEvent}>Clip</button>
-
+            <button type='button' onClick={addToClips}>Clip</button>
+            <button type='button' onClick={handleClipEvent}>10 secs</button>
+            
+            <Grid ref={grid} clips={clipState} clipEvent={handleClipEvent} />
+            
 
 
 
