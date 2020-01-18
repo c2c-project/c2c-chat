4b14902750036e1585570a9bc95780a0df42ddde
DevelopmentByDavid <david@yrbookit.com>
2020-01-17 23:54:32 -0800

    chore: 193 tf file created w/ description

---
+++ server/lib/tf.js
@@ -0,0 +1,12 @@
+// TODO: 193
+/**
+ * Stick all your tensorflow code here for now
+ * If you need more files feel free to create them.
+ *
+ * One thing you'll have to do is add the appropriate packages.
+ * Don't forget to use yarn instead of npm! If you're having trouble
+ * just contact me.
+ *
+ * Also, make sure you are adding these packages to the server's package.json and not the root or client's.
+ *
+ */
