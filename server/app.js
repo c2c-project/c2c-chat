95903434417a332359c263f71c0a5b35c4629064
DevelopmentByDavid <david@yrbookit.com>
2020-01-22 01:09:33 -0800

    chore: production stuff

--- server/app.js
+++ server/app.js
@@ -20,13 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(passport.initialize());
 
 app.use('/api/users', usersRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/sessions', sessionRouter);
 app.use('/api/questions', questionRouter);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
