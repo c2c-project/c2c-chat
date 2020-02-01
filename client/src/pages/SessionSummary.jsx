9622874adc1dfc1326d282fab97033c4a6876433
joepark1993 <jpark259@ucr.edu>
2020-02-01 18:47:01 -0500

    session stuff

--- client/src/pages/SessionSummary.jsx
+++ client/src/pages/SessionSummary.jsx
@@ -12,19 +12,77 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 
-
-
 export default function SessionSummary() {
-   const [data, setData] = React.useState([]);
-   const history = useHistory();
-   React.useEffect(() => {
-      fetch('/api/sessions/find').then(res => {
-         console.log.out("test");
-          res.json().then(r => setData(r));
-      });
-  });
+  const onSubmit = e => {
+   e.preventDefault();
+   fetch(`/api/sessions/${type}`, {
+       method: 'GET',
+       body:
+           type === 'create'
+               ? JSON.stringify({ form: state })
+               : JSON.stringify({ form: state, sessionId: editTarget }),
+       headers: {
+           'Content-Type': 'application/json'
+       }
+   }).then(() => {
+       // TODO: handle errors later
+       cb();
+   });
+};
       return(
          <div>{}</div>
       );
    
 }
+
+
+/*
+router.get(
+    '/find/:sessionId',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { sessionId } = req.params;
+        Sessions.findSessionById(sessionId).then(r => {
+            res.json(r);
+        });
+    }
+);
+    const onSubmit = e => {
+        e.preventDefault();
+        fetch(`/api/sessions/${type}`, {
+            method: 'POST',
+            body:
+                type === 'create'
+                    ? JSON.stringify({ form: state })
+                    : JSON.stringify({ form: state, sessionId: editTarget }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            // TODO: handle errors later
+            cb();
+        });
+    };
+    this is sent from sessions.js from route
+    router.post(
+    '/create',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { form } = req.body;
+        // TODO: add role checking
+        Sessions.addSession(form)
+            .then(() => res.send({ success: true }))
+            .catch(err => {
+                console.log(err);
+                res.send({ success: false });
+            });
+    }
+);
+
+    as you can see the type POST is sent to router.post and ${type} is create if
+    passed as a prop and used to fill in that URL `/api/sessions/${type}`
+
+
+
+
+*/
