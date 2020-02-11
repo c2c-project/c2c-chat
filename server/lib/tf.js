f1605e2603f672090fe7c55b85003d018f9745f9
Jia Xie <jxie031@ucr.edu>
2020-02-10 18:06:42 -0800

    chore: rewrite the structure, remove await and async

--- server/lib/tf.js
+++ server/lib/tf.js
@@ -1,15 +1,3 @@
-// TODO: 193
-/**
- * Stick all your tensorflow code here for now
- * If you need more files feel free to create them.
- *
- * One thing you'll have to do is add the appropriate packages.
- * Don't forget to use yarn instead of npm! If you're having trouble
- * just contact me.
- *
- * Also, make sure you are adding these packages to the server's package.json and not the root or client's.
- *
- */
 
 import '@tensorflow/tfjs-node';
 import * as toxicity from '@tensorflow-models/toxicity';
@@ -18,42 +6,39 @@ import Questions from '../db/collections/questions';
 
 const threshold = 0.9; // Will be change if the toxicity test is too sensitive.
 
-const toxicity_load = toxicity.load(threshold);// load toxicity before hand
-console.log("toxicity load success")
+const toxicityLoad = toxicity.load(threshold);// load toxicity before hand
 
 async function checkTfToxicity(question) {
-    const toxicityResult = {};
-    const toxicityReason = [];
-    let result = false;
-    try {
-        await toxicity_load.then(async model => {
-            await model.classify(question).then(async predictions => {
-                await predictions.forEach(prediction => {
+
+    const promise = new Promise(function(resolve) {
+        const toxicityResult = {};
+        const toxicityReason = [];
+        let result = false;
+        toxicityLoad.then(model => {
+            model.classify(question).then(predictions => {
+                predictions.forEach(prediction => {
                     // Remodel the value structure to a list of key-value pairs.
                     toxicityResult[prediction.label] =
                         prediction.results[0].match;
                 });
+                if (toxicityResult.toxicity) {
+                    for (let i = 0; i < Object.keys(toxicityResult).length - 1; i += 1) {
+                        // if value of toxicityResult is true or null, we add its key to the toxicityReason.
+                        if (!Object.values(toxicityResult)[i]) {
+                            toxicityReason.push(Object.keys(toxicityResult)[i]);
+                        }
+                    }
+                    result = true;
+                }
+                resolve({ toxicity: result, reason: toxicityReason })
             });
         });
-    } catch (exception) {
-        console.log({
-            message: 'check tf toxicity fail please checkout the tf connection'
-        });
-    }
-    if (toxicityResult.toxicity) {
-        for (let i = 0; i < Object.keys(toxicityResult).length - 1; i += 1) {
-            // if value of toxicityResult is true or null, we add its key to the toxicityReason.
-            if (!Object.values(toxicityResult)[i]) {
-                toxicityReason.push(Object.keys(toxicityResult)[i]);
-            }
-        }
-        result = true;
-    }
-    return { toxicity: result, reason: toxicityReason };
+    })
+    return promise;
 }
 
-async function AutoRemoveMessage(result, reason, messageId, io, roomId) {
-    await Chat.updateMessageToxicity({
+function AutoRemoveMessage(result, reason, messageId, io, roomId) {
+    Chat.updateMessageToxicity({
         messageId,
         result,
         toxicityReason: reason
@@ -67,29 +52,25 @@ async function AutoRemoveMessage(result, reason, messageId, io, roomId) {
 }
 
 async function tfToxicityQuestion(questionDoc) {
-    try {
-        if (questionDoc) {
-            const tfToxicityResult = await checkTfToxicity(
-                questionDoc.question
-            );
-            await Questions.updateQuestionToxicity({
+    if (questionDoc) {
+        checkTfToxicity(
+            questionDoc.question
+        ).then(tfToxicityResult =>{
+            Questions.updateQuestionToxicity({
                 questionId: questionDoc._id,
                 result: tfToxicityResult.toxicity,
                 toxicityReason: tfToxicityResult.reason
-            });
-        }
-    } catch (Exception) {
-        console.log(Exception);
+            })
+        })    
     }
 }
 
-async function tfToxicityMessage(messageDoc, io, roomId) {
-    try {
-        const messageId = messageDoc._id;
-        if (messageDoc) {
-            const tfToxicityResult = await checkTfToxicity(messageDoc.message);
+function tfToxicityMessage(messageDoc, io, roomId) {
+    const messageId = messageDoc._id;
+    if (messageDoc) {
+        checkTfToxicity(messageDoc.message).then(tfToxicityResult =>{
             if (tfToxicityResult.toxicity) {
-                await AutoRemoveMessage(
+                AutoRemoveMessage(
                     tfToxicityResult.toxicity,
                     tfToxicityResult.reason,
                     messageId,
@@ -98,8 +79,7 @@ async function tfToxicityMessage(messageDoc, io, roomId) {
                 );
             }
         }
-    } catch (Exception) {
-        console.log(Exception);
+        );
     }
 }
 
