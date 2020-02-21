7f3f6cc4b43ba81970892d65852dfe2b56b8b161
DevelopmentByDavid <david@yrbookit.com>
2020-02-21 11:03:17 -0800

    fix: storybook now works with mod dashboard

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,3 +1,6 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 
+addDecorator(storyFn => <BrowserRouter>{storyFn()}</BrowserRouter>);
 configure(require.context('../src', true, /\.stories\.jsx$/), module);
