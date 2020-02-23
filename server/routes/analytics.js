e7e046d845867f31642ca8cd8c337e8eaafe6689
DevelopmentByDavid <david@yrbookit.com>
2020-02-22 20:49:22 -0800

    refactor: added analytics module

---
+++ server/routes/analytics.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import passport from 'passport';
+import Analytics from '../lib/analytics';
+
+const router = express.Router();
+
+router.get(
+    '/session-summary/:sessionId',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { sessionId } = req.params;
+        // aggregate data and 
+        Analytics.summarizeSession(sessionId).then(summaryDetails => {
+            // send details to client
+        });
+    }
+);
+
+
+
+// Will get to this get request once chat is implemented
+
+module.exports = router;
