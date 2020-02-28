eec1d93c9c15276a37360d8d8c66242ddb2060de
Jia Xie <jxie031@ucr.edu>
2020-02-28 10:20:14 -0800

    chore: trello storyboard in 193 branch

---
+++ client/src/components/TrelloDashboard/Dialog.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function AlertDialog(props) {
+    return (
+        <div>
+            <Dialog
+                open={props.open}
+                onClose={props.handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title"> {props.data.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {props.data.description}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.handleClose} color="primary">
+                        Back
+                    </Button>
+                    <Button onClick={props.handleSetCurrent} color="primary" autoFocus>
+                        Set Current
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
