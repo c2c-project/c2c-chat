7673c2ce29688df6a7f669ac14f087b343778685
DevelopmentByDavid <dsilv022@ucr.edu>
2020-04-14 17:43:12 -0700

    refactor: socketio to use async/await

--- server/lib/socket-io.js
+++ server/lib/socket-io.js
@@ -1,7 +1,10 @@
 import socketio from 'socket.io';
-import JWT from 'jsonwebtoken';
+// import JWT from 'jsonwebtoken';
+import JWT from './jwt';
 import Messages from '../db/collections/messsages';
+import Log from './log';
 import tf from './tf';
+import { ClientError } from './errors';
 
 /**
  * all socket io stuff happens here, note that the server is not attached until it is created in bin/www.js
@@ -11,73 +14,54 @@ import tf from './tf';
 const io = socketio();
 io.serveClient(false);
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     // TODO: increment # of users connected here
     // eslint-disable-next-line
-    socket.on('disconnect', data => {
+    socket.on('disconnect', (data) => {
         // TODO: decrement # of users here
     });
 });
 
-/**
- * CHAT
- */
-
-// jwt is sent with every single chat message
-io.of('/chat').on('connection', socket => {
+io.of('/chat').on('connection', (socket) => {
     // roomId is just the sessionId -- we have different chatrooms for every session
     const { roomId } = socket.handshake.query;
-    // TODO: what happens if there's no roomId? maybe just explicitly nothing
     if (roomId) {
         socket.join(roomId);
-        socket.to(roomId).on('message', ({ message, jwt }) => {
-            JWT.verify(jwt, process.env.JWT_SECRET, (err, decodedJwt) => {
-                // TODO: how could this error? if so what do I do? idk atm
-                if (!err) {
-                    const { username, _id } = decodedJwt;
-                    Messages.createMessage({
-                        message,
-                        username,
-                        userId: _id,
-                        session: roomId,
-                        toxicity: false,
-                        toxicityReason : [],
-                    })
-                        .then(r => {
-                            // r is just the general object returned by the mongo driver when mongodb finishes
-                            // it has a property called ops, which is always an array of the inserted documents
-                            // since I am inserting one doc, then I only care about the first element
-                            // I want the object as returned by mongodb, so that I have the _id of the message in the db
-                            const messageDoc = r.ops[0];
-                            io.of('/chat')
-                                .to(roomId)
-                                .emit('message', messageDoc);
-                            tf.tfToxicityMessage(messageDoc, io, roomId);
-                        })
-                        .catch(e => console.log(e));
-                }
-            });
+        socket.to(roomId).on('message', async ({ message, jwt }) => {
+            try {
+                const { username, _id } = await JWT.verify(
+                    jwt,
+                    process.env.JWT_SECRET
+                );
+                const mongoCursor = await Messages.createMessage({
+                    message,
+                    username,
+                    userId: _id,
+                    session: roomId,
+                    toxicity: false,
+                    toxicityReason: [],
+                });
+                const myMessage = mongoCursor.ops[0]; // the message is the first index in ops property
+                io.of('/chat').to(roomId).emit('message', myMessage);
+                tf.tfToxicityMessage(myMessage, io, roomId);
+            } catch (e) {
+                Log.err(e);
+            }
         });
+    } else {
+        Log.err(new ClientError('No roomId'));
     }
 });
 
 export function moderate(roomId, messageId) {
-    io.of('/chat')
-        .to(roomId)
-        .emit('moderate', messageId);
+    io.of('/chat').to(roomId).emit('moderate', messageId);
 }
 
 export function unmoderate(roomId, message) {
-    io.of('/chat')
-        .to(roomId)
-        .emit('unmoderate', message);
+    io.of('/chat').to(roomId).emit('unmoderate', message);
 }
 
-/**
- * QUESTIONS
- */
-
-io.of('/questions').on('connection', socket => {
+io.of('/questions').on('connection', (socket) => {
     const { roomId } = socket.handshake.query;
     if (roomId) {
         socket.join(roomId);
@@ -85,9 +69,7 @@ io.of('/questions').on('connection', socket => {
 });
 
 export function setCurrentQuestion(sessionId, question) {
-    io.of('/questions')
-        .to(sessionId)
-        .emit('set-question', question);
+    io.of('/questions').to(sessionId).emit('set-question', question);
 }
 
 export default io;
