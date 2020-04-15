277ba9728d81983a11ac397eb7b66447390f5334
DevelopmentByDavid <dsilv022@ucr.edu>
2020-04-14 17:25:10 -0700

    docs: documented jwt module

--- server/lib/jwt.js
+++ server/lib/jwt.js
@@ -1,5 +1,11 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * @description wrapper to jsonwebtoken.verify
+ * @arg {string} token
+ * @arg {string} secret
+ * @returns {Promise} resolves to a decoded jwt on success
+ */
 const verify = async function (token, secret) {
     return new Promise((resolve, reject) => {
         jwt.verify(token, secret, (err, decodedJwt) => {
@@ -12,6 +18,13 @@ const verify = async function (token, secret) {
     });
 };
 
+/**
+ * @description wrapper to jsonwebtoken.sign
+ * @arg {Any} target this is going to jwt'd
+ * @arg {String} secret
+ * @arg {Object} [options] optional options for jwt signing
+ * @returns {Promise} resolves to the jwt on success
+ */
 const sign = async function (target, secret, options = {}) {
     return new Promise((resolve, reject) => {
         jwt.sign(target, secret, options, (err, token) => {
