79bbd313538fdf845a656efeabe729dc1ee6a56e
Kelton <kadey001@ucr.edu>
2020-04-21 16:27:16 -0700

    Added Rate Limiting to API Calls

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import createError from 'http-errors';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
@@ -34,6 +35,37 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
 }
+
+//Rate Limiters
+const masterLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 3000, // limit each IP to 3000 requests per windowMs
+    message: 'Exceeded over 3000 requests in 10 minutes'
+});
+
+const strictLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 20, // limit each IP to 20 requests per windowMs
+    message: 'Exceeded over 20 requests in 10 minutes',
+});
+
+const relaxedLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 1000, // limit each IP to 1000 requests per windowMs
+    message: 'Exceeded over 1000 requests in 10 minutes',
+});
+
+//Rate Limiting For All Routes
+app.use(masterLimiter);
+
+//Rate Limiting For Specific Routes
+app.use('/register', strictLimiter);
+app.use('/login', strictLimiter);
+app.use('/request-password-reset', strictLimiter);
+app.use('/consume-password-reset-token', strictLimiter);
+app.use('/verification', strictLimiter);
+app.use('/login-temporary', strictLimiter);
+app.use('/authenticate', relaxedLimiter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
